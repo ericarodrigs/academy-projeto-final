ee824412853d83a4b8e92e88fdba67e9c880ad91
Victor <carvalho.capanema@gmail.com>
2022-08-25 13:38:04 -0300

    Custom Elevated Button

---
+++ lib/w_system/atoms/custom_buttons.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'color_colors.dart';
+
+class WElevatedButton extends StatelessWidget {
+  const WElevatedButton({required this.text, this.function , Key? key}) : super(key: key);
+  final String text;
+  final Function()? function;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: function,
+      style: ElevatedButton.styleFrom(
+        primary: AppColors.deepPurple,
+        minimumSize: const Size(800, 20),
+        padding: const EdgeInsets.symmetric(vertical: 11),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        textStyle: const TextStyle(fontSize: 18),
+       ),
+      child: Text(text),
+    );
+  }
+}
