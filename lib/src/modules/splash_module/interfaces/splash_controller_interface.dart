fec935184b5f9fe8547598c6549fb727fe8ec0c1
marcos-willian <markimwrs@hotmail.com>
2022-08-30 23:56:42 +0000

    Revert "Merge branch 'login_model' into 'main'"
    This reverts merge request !7

--- lib/src/modules/splash_module/interfaces/splash_controller_interface.dart
+++
@@ -1,3 +0,0 @@
-abstract class ISplashController {
-  Future<void> tryLocalStorageLogin();
-}
--- lib/src/modules_route_names.dart
+++
@@ -1,4 +0,0 @@
-class ModulesRouteNames {
-  static const String splashModule = '/splash_module/';
-  static const String homeModule = '/home_module/';
-}
