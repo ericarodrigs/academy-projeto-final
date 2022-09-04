d0206c6d7fbcffc0dc0f21b555c2b578cb947e19
Victor <carvalho.capanema@gmail.com>
2022-09-04 17:39:43 -0300

    Carrossel videos relacionados

--- lib/src/modules/home/features/details/view/details_page.dart
+++ lib/src/modules/home/features/details/view/details_page.dart
@@ -1,6 +1,13 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:rarovideowall/src/modules/home/features/details/controller/video_details_controller.dart';
+import '../../../../../shared/constants/app_text_styles.dart';
+import '../../../../../w_system/atoms/texts/w_title_and_text.dart';
+import '../../../../../w_system/molecules/w_related_video_card.dart';
 
-class DetailsPage extends StatelessWidget {
+class DetailsPage extends StatefulWidget {
   const DetailsPage({
     Key? key,
     required this.videoId,
@@ -9,10 +16,84 @@ class DetailsPage extends StatelessWidget {
   final String videoId;
 
   @override
+  State<DetailsPage> createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+   final videoDetailsController = Modular.get<VideoDetailsController>();
+
+  @override
+  void initState() {
+    videoDetailsController.videoId = widget.videoId;
+    videoDetailsController.getVideo();
+    super.initState();
+
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(videoId),
+      body: SafeArea(
+        child: Observer(
+          builder: (_) => SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                children: [
+                  Text(videoDetailsController.video.nome, style: TextStyles.black26w700Urbanist),
+                  const SizedBox(height: 16),
+                  // videoDetailsController.loadState == LoadState.loading
+                  //     ? const Align(alignment: Alignment.center, child: CircularProgressIndicator())
+                  //     : AspectRatio(
+                  //         aspectRatio: 16 / 9, child: VideoPlayer(videoDetailsController.videoPlayercontroller!)),
+                  const SizedBox(height: 32),
+                  TitleAndText(title: 'Descrição', text: videoDetailsController.video.descricao),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text('Videos Relacionados', style: TextStyles.black26w700Urbanist),
+                      const SizedBox(height: 16),
+                      Visibility(
+                          visible: videoDetailsController.relatedState == LoadState.loading,
+                          child: const Align(alignment: Alignment.center, child: CircularProgressIndicator())),
+                      Visibility(
+                          visible: videoDetailsController.relatedState == LoadState.error,
+                          child: Column(
+                            children: [
+                              Text(videoDetailsController.relatedError, style: TextStyles.errorRed),
+                              ElevatedButton(
+                                  onPressed: () {
+                                    videoDetailsController.getRelatedVideos();
+                                  },
+                                  child: const Text('Recarregar'))
+                            ],
+                          )),
+                      Visibility(
+                        visible: videoDetailsController.relatedState == LoadState.done,
+                        child: CarouselSlider.builder(
+                          itemCount: videoDetailsController.relatedVideos.length,
+                          itemBuilder: (BuildContext context, int i, int pageViewIndex) => WvideoRelatedSlider(
+                            video: videoDetailsController.relatedVideos[i],
+                            onTap: () {
+                              Modular.to
+                                  .popAndPushNamed('details/${videoDetailsController.relatedVideos[i].id}');
+                            },
+                          ),
+                          options: CarouselOptions(
+                            aspectRatio: 2.0,
+                            enableInfiniteScroll: false,
+                            enlargeCenterPage: true,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 32),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/src/shared/repositories/videos_repository.dart
+++ lib/src/shared/repositories/videos_repository.dart
@@ -18,21 +18,40 @@ class VideosRepository {
     required this.loggedState,
   });
 
+  Future<VideoModel> getVideo(String id) async{
+    Response response = await service.request('/videos/$id', 'GET');
+    return VideoModel.fromMap(response.data);
+  }
+
+  Future<Either<Failure, List<VideoModel>>> getRelatedVideos(String topico) async {
+    try {
+      Response response = await service.request('/videos', 'GET', queryParams: {'topico': topico});
+      List<dynamic> videos = response.data;
+      List<VideoModel> newVideos = videos.map((video) => VideoModel.fromMap(video)).toList();
+      videosState.syncVideos(newVideos);
+      return Right(newVideos);
+    } on Failure catch (fail) {
+      return Left(fail);
+    } catch (err, stackTrace) {
+      return Left(
+        Failure(
+          'Erro inesperado',
+          object: err,
+          stackTrace: stackTrace,
+        ),
+      );
+    }
+  }
+
   Future<Either<Failure, List<VideoModel>>> getAllVideos() async {
     try {
       Response response = await service.request(
         '/videos',
         'GET',
-        queryParams: {
-          'pagina': 1,
-          'itensPorPagina': 100,
-          'orderBy': 'dataPublicacao',
-          'orderDirection': 'ASC',
-        },
+        queryParams: {'pagina': 1, 'itensPorPagina': 100, 'orderBy': 'dataPublicacao', 'orderDirection': 'ASC'},
       );
       List<dynamic> videos = response.data;
-      List<VideoModel> newVideos =
-          videos.map((video) => VideoModel.fromMap(video)).toList();
+      List<VideoModel> newVideos = videos.map((video) => VideoModel.fromMap(video)).toList();
       videosState.syncVideos(newVideos);
       return Right(newVideos);
     } on Failure catch (fail) {
@@ -56,16 +75,10 @@ class VideosRepository {
       Response response = await service.request(
         '/videos/favoritos',
         'GET',
-        queryParams: {
-          'pagina': 1,
-          'itensPorPagina': 100,
-          'orderBy': 'dataPublicacao',
-          'orderDirection': 'ASC',
-        },
+        queryParams: {'pagina': 1, 'itensPorPagina': 100, 'orderBy': 'dataPublicacao', 'orderDirection': 'ASC'},
       );
       List<dynamic> videos = response.data;
-      List<VideoModel> favoriteVideos =
-          videos.map((video) => VideoModel.fromMap(video)).toList();
+      List<VideoModel> favoriteVideos = videos.map((video) => VideoModel.fromMap(video)).toList();
       videosState.syncFavoriteVideos(favoriteVideos);
       return Right(favoriteVideos);
     } on Failure catch (fail) {
@@ -81,34 +94,34 @@ class VideosRepository {
     }
   }
 
-  // Future<Either<Failure, List<VideoModel>>> getPublicVideos() async {
-  //   try {
-  //     Response response = await service.request(
-  //       '/videos',
-  //       'GET',
-  //       queryParams: {
-  //         'pagina': 1,
-  //         'itensPorPagina': 100,
-  //         'orderBy': 'dataPublicacao',
-  //         'orderDirection': 'ASC',
-  //       },
-  //       headers: {'Authorization': ''},
-  //     );
-  //     List<dynamic> videos = response.data;
-  //     List<VideoModel> publicVideos =
-  //         videos.map((video) => VideoModel.fromMap(video)).toList();
-  //     return Right(publicVideos);
-  //   } on Failure catch (fail) {
-  //     return Left(fail);
-  //   } catch (err, stackTrace) {
-  //     return Left(
-  //       Failure(
-  //         'Erro inesperado',
-  //         object: err,
-  //         stackTrace: stackTrace,
-  //       ),
-  //     );
-  //   }
-  // }
+// Future<Either<Failure, List<VideoModel>>> getPublicVideos() async {
+//   try {
+//     Response response = await service.request(
+//       '/videos',
+//       'GET',
+//       queryParams: {
+//         'pagina': 1,
+//         'itensPorPagina': 100,
+//         'orderBy': 'dataPublicacao',
+//         'orderDirection': 'ASC',
+//       },
+//       headers: {'Authorization': ''},
+//     );
+//     List<dynamic> videos = response.data;
+//     List<VideoModel> publicVideos =
+//         videos.map((video) => VideoModel.fromMap(video)).toList();
+//     return Right(publicVideos);
+//   } on Failure catch (fail) {
+//     return Left(fail);
+//   } catch (err, stackTrace) {
+//     return Left(
+//       Failure(
+//         'Erro inesperado',
+//         object: err,
+//         stackTrace: stackTrace,
+//       ),
+//     );
+//   }
+// }
 
 }
