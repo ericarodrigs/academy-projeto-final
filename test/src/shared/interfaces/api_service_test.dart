8e8c5c66cf8367b31fa00050426806e07f43b579
Marcos Willlian <markimwrs@hotmail.com>
2022-09-09 01:47:02 -0300

    create mocks api_service

--- test/src/shared/interfaces/api_service_test.dart
+++ test/src/shared/interfaces/api_service_test.dart
@@ -3,16 +3,73 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:rarovideowall/src/shared/interfaces/api_service.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:rarovideowall/src/shared/models/failure.dart';
 
 import 'api_service_test.mocks.dart';
 
-@GenerateMocks([ApiService])
+@GenerateMocks([ApiService<Response>])
 void main() {
-  late ApiService<Response> service;
+  late ApiService service;
   setUpAll(() {
-    service = MockApiService<Response>();
+    service = MockApiService();
   });
-  group('Test api service', () async {
-    test('')
+  group('Test api service', () {
+    test('Should return a instance of Response when mode is POST', () async {
+      when(service.request('/test', 'POST')).thenAnswer(
+        (_) async => Response(
+          requestOptions: RequestOptions(path: 'test'),
+        ),
+      );
+      final response = await service.request('/test', 'POST');
+      expect(response, isInstanceOf<Response>());
+    });
+    test('Should return a instance of Response when mode is GET', () async {
+      when(service.request('/test', 'GET')).thenAnswer(
+        (_) async => Response(
+          requestOptions: RequestOptions(path: 'test'),
+        ),
+      );
+      final response = await service.request('/test', 'GET');
+      expect(response, isInstanceOf<Response>());
+    });
+    test('Should return a instance of Response when mode is PATCH', () async {
+      when(service.request('/test', 'PATCH')).thenAnswer(
+        (_) async {
+          return Response(
+            requestOptions: RequestOptions(path: 'test'),
+          );
+        },
+      );
+      final response = await service.request('/test', 'PATCH');
+      expect(response, isInstanceOf<Response>());
+    });
+    test('Should return a instance of Response when mode is PUT', () async {
+      when(service.request('/test', 'PUT')).thenAnswer(
+        (_) async => Response(
+          requestOptions: RequestOptions(path: 'test'),
+        ),
+      );
+      final response = await service.request('/test', 'PUT');
+      expect(response, isInstanceOf<Response>());
+    });
+    test('Should return a instance of Response when mode is DELETE', () async {
+      when(service.request('/test', 'DELETE')).thenAnswer(
+        (_) async => Response(
+          requestOptions: RequestOptions(path: 'test'),
+        ),
+      );
+      final response = await service.request('/test', 'DELETE');
+      expect(response, isInstanceOf<Response>());
+    });
+    test('Should return throw an exception of Failure if mode is wrong',
+        () async {
+      when(service.request('/test', 'POT')).thenThrow(Failure('test'));
+      try {
+        await service.request('/test', 'POT');
+      } on Failure catch (err) {
+        expect(err, isInstanceOf<Failure>());
+        expect(err.message, Failure('test').message);
+      }
+    });
   });
 }
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\DevPrograms\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\marki\\OneDrive - Universidade Federal de Minas Gerais\\Livros_e_Faculdade\\Progaming\\Dart_RARO\\Exercicios\\proj_final\\rarovideowall" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\DevPrograms\\flutter"
+  "PROJECT_DIR=C:\\Users\\marki\\OneDrive - Universidade Federal de Minas Gerais\\Livros_e_Faculdade\\Progaming\\Dart_RARO\\Exercicios\\proj_final\\rarovideowall"
+  "FLUTTER_ROOT=C:\\DevPrograms\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\marki\\OneDrive - Universidade Federal de Minas Gerais\\Livros_e_Faculdade\\Progaming\\Dart_RARO\\Exercicios\\proj_final\\rarovideowall\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\marki\\OneDrive - Universidade Federal de Minas Gerais\\Livros_e_Faculdade\\Progaming\\Dart_RARO\\Exercicios\\proj_final\\rarovideowall"
+  "FLUTTER_TARGET=C:\\Users\\marki\\OneDrive - Universidade Federal de Minas Gerais\\Livros_e_Faculdade\\Progaming\\Dart_RARO\\Exercicios\\proj_final\\rarovideowall\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\marki\\OneDrive - Universidade Federal de Minas Gerais\\Livros_e_Faculdade\\Progaming\\Dart_RARO\\Exercicios\\proj_final\\rarovideowall\\.dart_tool\\package_config.json"
+)
